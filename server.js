94d3ea4a635855546ba870b1f7329a7c12bf5af5
kshitij2022 <kshitijwohlig@gmail.com>
2022-07-19 18:09:48 +0530

    first commit

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 const express = require('express')
 const app = express()
 // const PORT = 3000
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
 const mongoose  = require('mongoose')
 const {PORT,mongoUri} = require('./config')
 const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser') 
 const bucketListItemRoutes = require('./routes/api/bucketListItems')
-const path = require('path')
+// const path = require('path')
 
 
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
-dotenv.config({path:'./.env'})
+// dotenv.config({path:'./.env'})
 
 
 
 app.use('/api/bucketListItems', bucketListItemRoutes)
 
-// app.get('/',(req,res)=>{
-//     res.send("Hello World")
-// })
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
 
 
 // horeku start
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/dist'))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+//     })
+// }
 // horeku end
 
 app.listen(PORT,()=>{
